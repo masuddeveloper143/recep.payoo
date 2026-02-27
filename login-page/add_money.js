dc625d41ff4a9bc4734e8e372147030c0f4ee8ac
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-28 02:27:11 +0800

    Update project

--- login-page/add_money.js
+++ login-page/add_money.js
@@ -8,10 +8,19 @@ document.getElementById('add-money_box')
         document.getElementById('cashout-section').style.display = "none";
     })
 
-    document.getElementById('cashout_box')
+document.getElementById('cashout_box')
     .addEventListener('click', function (event) {
         event.preventDefault();
         document.getElementById('cashout-section').style.display = "block";
         document.getElementById('first_login_section').style.display = "none";
     })
-     
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addAmount = document.getElementById('add_money_amount').value;
+        const addPin = document.getElementById('amount').value;
+        const addMoneyMinBalancr = document.getElementById('add_money_min_balence').innerText;
+    
+    })
