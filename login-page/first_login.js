ef2de90931aede8f4a7fe464bec61e650d826a69
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-28 00:56:45 +0800

    Update project

--- login-page/first_login.js
+++ login-page/first_login.js
@@ -6,5 +6,22 @@ document.getElementById('btn-login')
 
         const acountNumber = document.getElementById('input-number').value;
         // console.log(typeof acountNumber); 
-        const pinNmber = document.getElementById('')
+        const pinNmber = document.getElementById('input-pin');
+
+        const convertedNumber = parseInt(acountNumber);
+        const convertePin = parseInt(pinNmber);
+
+        if (convertedNumber === 11) {
+
+            if (convertePin === 1234) {
+                alert('successfull login 🎉');
+            }
+            else {
+                alert('incorrct your pin');
+            }
+        }
+
+        else {
+            alert('incorrct your number')
+        }
     })
