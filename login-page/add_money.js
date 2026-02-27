157f912d8ac5bf4ee665442479cee334b466c714
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-28 03:14:50 +0800

    Update project

--- login-page/add_money.js
+++ login-page/add_money.js
@@ -19,16 +19,28 @@ document.getElementById('btn-add-money')
     .addEventListener('click', function (event) {
         event.preventDefault();
 
+        const addNumber = document.getElementById('add-money_number').value;
         const addAmount = document.getElementById('add_money_amount').value;
-        const addPin = document.getElementById('amount').value;
+        const addPin = document.getElementById('add_money_pin').value;
         const addMoneyMinBalance = document.getElementById('add_money_min_balence').innerText;
 
         // converted area 
-        if ()
-            const convertedaddAmount = parseFloat(addAmount);
+        const convertedaddAmount = parseFloat(addAmount);
         const convertedaddPin = parseFloat(addPin);
         const convertedaddMoneyMinBalance = parseFloat(addMoneyMinBalance);
+        // console.log(convertedaddAmount, convertedaddPin, addNumber);
+        if (addNumber.length === 11 && addNumber.startsWith("01") && convertedaddPin === 1234) {
+
+            alert('successful add money🎉');
+            const sum = convertedaddAmount + convertedaddMoneyMinBalance;
+            document.getElementById('add_money_min_balence').innerText = sum;
+
+        }
+
+        else {
+            alert('invalid ');
+        }
 
 
 
-    })
+    });
