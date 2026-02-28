e78cb92b78b6d69be3bf44395193738d2bd663f3
Masud Rana <md.masudrana.developer@gmail.com>
2026-02-28 14:42:13 +0800

    Update project

--- login-page/cashout.js
+++ login-page/cashout.js
@@ -1,6 +1,28 @@
 
 
 document.getElementById('cashout_btn')
-.addEventListener('click', function(event){
-    console.log('cliked button');
-});
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        // input value
+
+        const cashoutNumber = document.getElementById('cashout_number');
+        const cashoutAmount = document.getElementById('cashout_amount');
+        const cashoutPin = document.getElementById('cashout_pin');
+        const cashoutMinBalance = document.getElementById('add_money_min_balence').innerText;
+
+        // coverted area
+
+        const converdcashoutAmount = parseFloat(cashoutAmount);
+        const convertedcashoutPin = parseFloat(cashoutPin);
+        const convertedcashoutMinBalance = parseInt(cashoutMinBalance);
+
+        if (cashoutNumber.length === 11 && cashoutNumber.startsWith("01")) {
+
+            console.('number is oky');
+        }
+        else {
+            alert('incorrect number');
+        }
+
+    });
